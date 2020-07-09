aed11d16ba5ef0b89523364322d94e1dd63bbfb0
mohemohe <mohemohe@ghippos.net>
2020-07-09 22:05:29 +0900

    ﾁｩﾌ

--- server/util/webcam.go
+++ server/util/webcam.go
@@ -115,7 +115,7 @@ func byteToJpeg(b []byte, width int, height int) []byte {
 	}
 
 	buf := &bytes.Buffer{}
-	if err := jpeg.Encode(buf, yuyv, nil); err != nil {
+	if err := jpeg.Encode(buf, yuyv, &jpeg.EncoderOptions{Quality: 96}); err != nil {
 		log.Error(err)
 		return []byte{}
 	}
